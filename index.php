c77df2649ed89e73c619cd3c14c47d7f67b2b06b
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2020-02-20 14:16:36 +0100

    utf support, random name+lastname pairs

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <?php 
 $db = new mysqli('localhost', 'root', '', 'identity');
-
+$db->set_charset('utf8');
 $q = "SELECT * FROM firstName";
 $result = $db->query($q); //result zawiera "odpowiedz" serwera sql
 $firstName = array();
@@ -31,6 +31,15 @@ while($row = $result->fetch_assoc()) { //dopóki możesz wyciągnąć kolejny wi
 echo '<pre>';
 print_r($firstName);
 print_r($lastName);
+
+for($i = 0 ; $i < 10 ; $i++) {
+    $randomId = $firstName[rand(0,count($firstName)-1)];
+    $randomId .= " ";
+    $randomId .= $lastName[rand(0,count($lastName)-1)];
+
+    echo $randomId;
+    echo '<br>';
+}
 
 ?>
 
