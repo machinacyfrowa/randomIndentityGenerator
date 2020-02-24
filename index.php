c46ffe460d713098cb125ab33973d1bfb68c944e
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2020-02-24 13:30:26 +0100

    finished function

--- index.php
+++ index.php
@@ -41,11 +41,14 @@ for($i = 0 ; $i < 10 ; $i++) {
     echo '<br>';
 }
 
-function generujPESEL($plec) {
+function generujPESEL(string $plec) : string
+{
     //$plec = {'m','k'}
     $rok = rand(20,99);
     $miesiac = rand(1,12);
+    $miesiac = str_pad($miesiac, 2, '0', STR_PAD_LEFT);
     $dzien = rand(1,28);
+    $dzien = str_pad($dzien, 2, '0', STR_PAD_LEFT);
     $porzadkowe = rand(100,999);
     $nieparzyste = array(1,3,5,7,9);
     $plecLiczba = $nieparzyste[rand(0,4)]; //losowa nieparzysta
@@ -53,8 +56,16 @@ function generujPESEL($plec) {
         $plecLiczba --;
     $pesel = $rok.$miesiac.$dzien.$porzadkowe.$plecLiczba; //string 10 znakowy
     $pesel = str_split($pesel); //tablica 10 elementowa
-    
+    $suma = 0;
+    $suma += $pesel[0]*9 + $pesel[1]*7 + $pesel[2]*3 + $pesel[3]*1 + $pesel[4]*9;
+    $suma += $pesel[5]*7 + $pesel[6]*3 + $pesel[7]*1 + $pesel[8]*9 + $pesel[9]*7;
+    $suma = $suma % 10;
+    array_push($pesel, $suma);
+    $pesel = implode("", $pesel);
+    return $pesel;
 }
+
+echo generujPESEL('k');
 
 ?>
 
